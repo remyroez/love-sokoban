a6ac0ae288013b4c044899cf263961f313caa420
れみろう <remyroez+github@gmail.com>
2019-04-24 21:17:23 +0900

    :pencil: Title シーンの追加
    - フォントの追加

--- game/src/Scene/Boot.lua
+++ game/src/Scene/Boot.lua
@@ -2,18 +2,26 @@
 local folderOfThisFile = (...):match("(.-)[^%/%.]+$")
 local Scene = require(folderOfThisFile .. 'Scene')
 
+-- エイリアス
+local lg = love.graphics
+
 -- ブート
 local Boot = Scene:addState('boot', Scene)
 
 -- 読み込み
 function Boot:load()
     -- 画面のサイズ
-    local width, height = love.graphics.getDimensions()
+    local width, height = lg.getDimensions()
     self.width = width
     self.height = height
 
     -- スプライトシートの読み込み
     self.sprite = sbss:new('assets/spritesheet.xml')
+
+    -- フォント
+    local font = 'assets/Kenney Future Square.ttf'
+    self.font64 = lg.newFont(font, 64)
+    self.font16 = lg.newFont(font, 16)
 
     -- レベルデータ
     self.levels = love.filesystem.load('assets/levels.lua')()
@@ -23,6 +31,6 @@ end
 
 -- 更新
 function Boot:update()
-    -- レベル選択へ
-    self:gotoState 'select'
+    -- タイトルへ
+    self:gotoState 'title'
 end
---
+++ game/src/Scene/Title.lua
@@ -0,0 +1,39 @@
+
+local folderOfThisFile = (...):match("(.-)[^%/%.]+$")
+local Scene = require(folderOfThisFile .. 'Scene')
+
+-- エイリアス
+local lg = love.graphics
+
+-- タイトル
+local Title = Scene:addState('title', Scene)
+
+-- 読み込み
+function Title:load()
+end
+
+-- 更新
+function Title:update(dt)
+end
+
+-- 描画
+function Title:draw()
+    -- クリア
+    lg.clear(.42, .75, .89)
+
+    -- タイトル
+    lg.setColor(1, 1, 1)
+    lg.printf("SOKOBAN", self.font64, 0, self.height * 0.3, self.width, 'center')
+
+    -- キー入力待ち描画
+    --lg.setColor(1.0, 0, 0.25)
+    lg.printf("PRESS ANY KEY", self.font16, 0, self.height * 0.6, self.width, 'center')
+end
+
+-- キー入力
+function Title:keypressed(key, scancode, isrepeat)
+    if key == 'return' then
+        -- レベル選択へ
+        self:gotoState 'select'
+    end
+end
